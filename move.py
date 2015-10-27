7dc1d4da3f87c659f419d0092038f6951c477dd5
Zack Baldwin <zack@zackb.com>
2015-10-26 19:34:58 -0500

    cleaned up code, 4 space tab, 80 char max, added the start of logging

--- move.py
+++ move.py
@@ -4,7 +4,19 @@ import fnmatch
 import exifread
 import shutil
 from datetime import datetime
-import time
+import logging
+
+# Logging
+LOG_FILENAME = 'move_log.out'
+logging.basicConfig(format='%(asctime)s - %(levelname)s:%(message)s',
+                    filename=LOG_FILENAME,
+                    level=logging.DEBUG)
+
+# logging.debug('This is a debug message')
+# logging.info('This is an info message')
+# logging.warning('This is a warning message')
+# logging.error('This is an error message')
+# logging.critical('This is a critical error message')
 
 # Config
 deleteSrcFile = False
@@ -12,81 +24,95 @@ mediaPath = "./img"
 dest_path = "./dest"
 fileTypes = ["*.jpg", "*.cr2", "*.raw", "*.nef"]
 
-#Variables
+# Variables
 Images = []
 
-class Image(object):
-  def __init__(self, srcPath, fileName, shotDate='', destPath=''):
-    self.srcPath = srcPath
-    self.destPath = destPath
-    self.fileName = fileName
-    self.shotDate = shotDate
 
-  def __str__(self):
-    returnStr =  'Date:' + str(self.shotDate) +'\n'
-    returnStr += 'src :' + str(self.srcPath) +'\n'
-    returnStr += 'dest:' + str(self.destPath) +'\n'
-    returnStr += 'file:' + str(self.fileName) +'\n'
-    return returnStr
+class Image(object):
+
+    def __init__(self, srcPath, fileName, shotDate='', destPath=''):
+        self.srcPath = srcPath
+        self.destPath = destPath
+        self.fileName = fileName
+        self.shotDate = shotDate
+
+    def __str__(self):
+        returnStr = 'Date:' + str(self.shotDate) + '\n'
+        returnStr += 'src :' + str(self.srcPath) + '\n'
+        returnStr += 'dest:' + str(self.destPath) + '\n'
+        returnStr += 'file:' + str(self.fileName) + '\n'
+        return returnStr
+
 
 # Methods
 def findImages():
-  for root, dirnames, filenames in os.walk(mediaPath):
-    for filename in filenames:
-      if matchesExtensions(filename):
-        srcPath = root
-        shotDate = getShotDate(os.path.join(root, filename))
-     
-        if isinstance(shotDate, exifread.classes.IfdTag):
-          destPath = os.path.join(dest_path, datetime.strptime(str(shotDate), "%Y:%m:%d %H:%M:%S").strftime("%Y/%Y-%m-%d Rename Me"))
-        else:
-          destPath = os.path.join(dest_path, 'unsorted')
-        Images.append(Image(srcPath, filename, shotDate, destPath))
-  return
+    logging.info('This is an info message')
+    for root, dirnames, filenames in os.walk(mediaPath):
+        for filename in filenames:
+            if matchesExtensions(filename):
+                srcPath = root
+                shotDate = getShotDate(os.path.join(root, filename))
+
+            if isinstance(shotDate, exifread.classes.IfdTag):
+                destPath = os.path.join(dest_path, datetime.strptime(str(shotDate), "%Y:%m:%d %H:%M:%S").strftime("%Y/%Y-%m-%d Rename Me"))
+            else:
+                destPath = os.path.join(dest_path, 'unsorted')
+                Images.append(Image(srcPath, filename, shotDate, destPath))
+    logging.info('Found ' + Images.length + ' Images')
+    return
+
 
 def matchesExtensions(name):
-  for pattern in fileTypes:
-    if fnmatch.fnmatch(name.lower(), pattern.lower()):
-      return True
-  return False
+    for pattern in fileTypes:
+        if fnmatch.fnmatch(name.lower(), pattern.lower()):
+            return True
+    return False
+
 
 def moveImages():
-  for Image in Images:
-    transferFile(Image.srcPath, Image.destPath, Image.fileName)
+    for Image in Images:
+        transferFile(Image.srcPath, Image.destPath, Image.fileName)
+
 
 def transferFile(src, dest, fileName):
-  if not os.path.isdir(dest):
-    os.makedirs(dest)
-    print('11111')
-  if deleteSrcFile == False:
-    shutil.copy(os.path.join(src, fileName), os.path.join(dest, fileName))
-  else:
-    shutil.move(os.path.join(src, fileName), os.path.join(dest, fileName))
+    if not os.path.isdir(dest):
+        os.makedirs(dest)
+    # TODO: error handling
+
+    if deleteSrcFile:
+        shutil.move(os.path.join(src, fileName), os.path.join(dest, fileName))
+
+    # TODO: Logging
+    else:
+        shutil.copy(os.path.join(src, fileName), os.path.join(dest, fileName))
+
 
 def getShotDate(filePath):
-  # Open image file for reading (binary mode)
-  try:
-    f = open(filePath, 'rb')
-
+    # Open image file for reading (binary mode)
     try:
-      # Return Exif tags
-      tags = exifread.process_file(f, details=False, stop_tag="EXIF DateTimeOriginal")
-      shotDate = tags["EXIF DateTimeOriginal"]
-    except (KeyError) as e:
-      return
-    finally:
-      f.close()
-    return shotDate
+        f = open(filePath, 'rb')
 
-  except (IOError, OSError) as e:
-    return 
+        try:
+            # Return Exif tags
+            tags = exifread.process_file(f, details=False,
+                                         stop_tag="EXIF DateTimeOriginal")
+            shotDate = tags["EXIF DateTimeOriginal"]
+        except (KeyError):
+            return
+        finally:
+            f.close()
+        return shotDate
+
+    except (IOError, OSError):
+        logging.error('Could not open' + filePath)
+    return
+
 
 def main():
-  #print(datetime.datetime.today().strftime("%Y\%m\%d"))
-  findImages()
-  moveImages()
-  print(*Images, sep='\n\n')
+    findImages()
+    moveImages()
+    print(*Images, sep='\n\n')
 
 
 if __name__ == "__main__":
-  main()
+    main()
